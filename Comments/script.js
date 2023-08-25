039d4328447ddd1fbfa6e0e2e574fdea38b6c0fe
Jagadeesh <110707772+Jagadeesh3996@users.noreply.github.com>
2023-08-25 08:25:58 +0530

    Comments page uploeded

---
+++ Comments/script.js
@@ -0,0 +1,28 @@
+function updatePostsList(posts) {
+    const postsDiv = document.getElementById('posts');
+    postsDiv.innerHTML = '';
+    posts.forEach(post => {
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+        postDiv.innerHTML = `<br><strong>Name:</strong> ${post.name}<br><strong>E-mail:</strong> ${post.email}<br><strong>Comments:</strong> ${post.body}<br><br>`;
+        postsDiv.appendChild(postDiv);
+    });
+}
+
+function getPosts() {
+    const numRecords = document.getElementById('numRecords').value;
+
+    fetch(`https://jsonplaceholder.typicode.com/comments?_limit=${numRecords}`)
+        .then(response => response.json())
+        .then(data => {
+            updatePostsList(data);
+            console.log(`${numRecords} posts fetched:`, data);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function clearPosts() {
+    const postsDiv = document.getElementById('posts');
+    postsDiv.innerHTML = '';
+    console.log('Posts cleared');
+}
